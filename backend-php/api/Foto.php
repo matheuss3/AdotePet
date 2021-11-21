ebfe0c47e5c563c21f1ff6f83c8283f078196cc4
Matheus Costa <41810688+MatheusHGCosta@users.noreply.github.com>
2021-11-21 20:08:20 -0300

    Foto.php

---
+++ backend-php/api/Foto.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @author Matheus
+ */
+require_once $_SERVER['DOCUMENT_ROOT'] . "/api/model/foto.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/api/rotas/IRouter.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/api/rotas/Router.php";
+
+//padrao de projetos strategy   
+class Animais implements IRouter {
+
+    public function get() {
+     
+       $Arrayresposta = array();
+       $foto = array();
+       $foto["descricao"] = "Foto do animal";     
+       
+       
+       array_push($ArrayResposta ,$foto);
+       echo json_encode($ArrayResposta);
+
+    }
+
+    public function post() {
+        $foto = new Foto();
+        $foto->set_descricao($_POST['descricao']);
+        /*
+         * Salvar no banco de dados
+         */
+
+    }
+
+    public function put() {
+        http_response_code(404);
+        throw new Exception("Não implementado ainda");
+    }
+
+    public function delete() {
+        http_response_code(404);
+        throw new Exception("Não implementado ainda");
+    }
+
+}
+
+header("Access-Control-Allow-Origin: *");
+$router = new Router(new foto());
+$router->run();
