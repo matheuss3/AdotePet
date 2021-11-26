3034a86347ac9e93a54650266035881ccb55c38f
luizadealencar <66708154+luizadealencar@users.noreply.github.com>
2021-11-26 10:46:44 -0300

    Add files via upload

--- AdotePetJavaProjetct/src/dao/IPessoaDao.java
+++ AdotePetJavaProjetct/src/dao/IPessoaDao.java
@@ -1,10 +1,8 @@
-package dao;
 import java.util.List;
 
 public interface IPessoaDao {
-
     public List<Pessoa> getAllPessoas();
-    public Pessoa getPessoa(int cpf);
+    public Pessoa getPessoa(String cpf);
     public void updatePessoa(Pessoa pessoa);
     public void deletePessoa(Pessoa pessoa);
 }
--- AdotePetJavaProjetct/src/dao/PessoaDaoImpl.java
+++ AdotePetJavaProjetct/src/dao/PessoaDaoImpl.java
@@ -1,6 +1,3 @@
-package dao;
-
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,29 +11,27 @@ public class PessoaDaoImpl implements IPessoaDao {
         pessoas = new ArrayList<Pessoa>();
 
         //Utilizando apenas de teste, substituir em breve
-        Pessoa pessoa1 = new Pessoa("Marcus","12345");
-        Pessoa pessoa2 = new Pessoa("Guilherme", "45678");
+        Pessoa pessoa1 = new Pessoa("Fulana", "1234567890", "rua poo2", "fulana@gmail.com", "987456123", "01/02/2000");
+        Pessoa pessoa2 = new Pessoa("Fulana", "1234567890", "rua poo2", "fulana@gmail.com", "987456123", "01/02/2000");
 
         pessoas.add(pessoa1);
-        pessoa1.add(pessoa2);
+        pessoas.add(pessoa2);
     }
 
     @Override
     public void deletePessoa(Pessoa pessoa) {
-        pessoas.remove(pessoa.getCpf());
-        System.out.println("Pessoa de cpf "+pessoa.getCpf()+", deletada!");
+        pessoas.remove(Pessoa.getCpf());
+        System.out.println("Pessoa de cpf "+ pessoa.getCpf()+ ", deletada!");
     }
 
     //recebe a lista de pessoas de um banco de dados
     @Override
-    public List<Pessoas> getAllPessoas() {
+    public List<Pessoa> getAllPessoas() {
         return pessoas;
     }
 
     @Override
-    public Pessoa getPessoa(string cpf){
+    public Pessoa getPessoa(String cpf){
         return pessoas.get(cpf);
     }
-
-
 }
