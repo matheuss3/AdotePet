90544cedb1fd8eb978c147bfc356212fdfa20401
Matheus Costa <41810688+MatheusHGCosta@users.noreply.github.com>
2021-11-28 21:48:03 -0300

    Update Pessoa.php

--- backend-php/api/Pessoa.php
+++ backend-php/api/Pessoa.php
@@ -5,6 +5,7 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . "/api/model/Pessoa.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/api/rotas/IRouter.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/api/rotas/Router.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/api/persistencia/FotoMapper.php";
 
 //padrao de projetos strategy   
 class Animais implements IRouter {
@@ -39,6 +40,10 @@ class Animais implements IRouter {
         /*
          * Salvar no banco de dados
          */
+        
+        $animalMapper = new FotoMapper();
+
+        $animalMapper->salvar($humano);
 
     }
 
