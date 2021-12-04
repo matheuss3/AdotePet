ab8569347dba8366fe251822bc5f5599857f4f02
thaisdesouzaa <79648866+thaisdesouzaa@users.noreply.github.com>
2021-12-04 11:29:20 -0300

    Adicionando os models Adotante, Doador e PedidoAdocao

---
+++ backend-php/api/model/Doador.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * @author Thaís
+ */
+class Doador {
+   
+    private $nome;
+    private $cpf;
+    private $dataNascimento;
+    private $endereco;
+    private $email;
+    private $telefone;
+   
+    public function get_nome() {
+        return $this->nome;
+    }
+
+    public function get_cpf() {
+        return $this->cpf;
+    }
+
+    public function get_dataNascimento() {
+        return $this->dataNascimento;
+    }
+    
+    public function get_endereco() {
+        return $this->endereco;
+    }
+    
+    public function get_email() {
+        return $this->email;
+    }
+
+    public function get_telefone() {
+        return $this->telefone;
+    }
+
+    public function set_nome($nome) {
+        $this->nome = $nome;
+        return $this;
+    }
+    
+    public function set_cpf($cpf) {
+        $this->cpf = $cpf;
+        return $this;
+    }
+
+    public function set_dataNascimento($dataNascimento) {
+        $this->dataNascimento = $dataNascimento;
+        return $this;
+    }
+    
+    public function set_endereco($endereco) {
+        $this->endereco = $endereco;
+        return $this;
+    }
+    
+    public function set_email($email) {
+        $this->email = $email;
+        return $this;
+    }
+    
+    public function set_telefone($telefone) {
+        $this->telefone = $telefone;
+        return $this;
+    }  
+
+}
