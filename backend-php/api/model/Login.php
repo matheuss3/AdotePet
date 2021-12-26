f41c5bb22bb077eabf59d9f515b5a96b83c1c312
Matheus Costa <41810688+MatheusHGCosta@users.noreply.github.com>
2021-12-26 10:50:18 -0300

    Create Login.php
    Criação da classe Login

---
+++ backend-php/api/model/Login.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @author Matheus
+ */
+class Login {
+   
+    private $login; 
+    private $senha; 
+
+
+    public function get_senha() {
+        return $this->senha;
+    }
+    
+    public function set_senha($senha) {
+        $this->senha = $senha;
+        return $this;
+    }
+
+    public function get_login() {
+        return $this->login;
+    }
+    
+    public function set_login($login) {
+        $this->login = $login;
+        return $this;
+    }
+ 
+}
